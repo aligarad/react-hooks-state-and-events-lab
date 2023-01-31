9f13ea6d136d81bb8e007ebb67fabde22dc85f39
aligarad <garadcali190@gmail.com>
2023-01-31 14:19:07 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,24 @@
-import React from "react";
-import React, {useState}  from "react"
+import React, {useState} from "react";
 import ShoppingList from "./ShoppingList";
 import itemData from "../data/items";
 
 function App() {
-
-  // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
   const [isLight, setIsDark] = useState(false)
   console.log({isLight, setIsDark})
-  
 
   function changeBgColor (){
     setIsDark(!isLight)
-    
+    console.log('hi')
+
   }
-
-    const appClass = false ? "App dark" : "App light"
-    const appClass = isLight ? "App dark" : "App light"
-
+  // replace 'false' with a state variable that can be toggled between true and false
+  // this will be used for the Dark Mode Toggle feature
+  const appClass = isLight ? "App dark" : "App light"
   return (
     <div className={appClass}>
       <header>
         <h2>Shopster</h2>
-        <button>Dark Mode</button>
-        <button onClick={changeBgColor}>Dark Mode</button>
+        <button onClick={changeBgColor} >Dark Mode</button>
       </header>
       <ShoppingList items={itemData} />
     </div>
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,26 +1,24 @@
 import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, addCart] = useState(false)
+  console.log(inCart)
 
-     const [inCart, addCart] = useState(false)
+  function addToCart(){
+    addCart((inCart)=>! inCart)
+  }
 
+  const cartState = inCart ? "in-cart" : "" 
+  const cartStateText = inCart ? "Remove From Cart" : "Add to Cart" 
 
-     function addToCart(){
-      addCart((inCart) =>! inCart)
-     }
-
-
-     const cartState = inCart ? "in-cart" : ""
-     const cartStateText = inCart ? "Remove From Cart" : "Add to Cart"
 
   return (
-      <li className={cartState}>
+    <li className={cartState}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
       <button onClick={addToCart} className="add">{cartStateText}</button>
-      </li>
-  
+
+    </li>
   );
 }
 
